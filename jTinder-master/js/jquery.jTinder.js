11f1bff4e2f67bb667d3716dade9df40acfddd5a
Jake Si <jake.si@live.com>
2016-01-17 08:19:01 -0500

    made pull new pics from flickr

--- jTinder-master/js/jquery.jTinder.js
+++ jTinder-master/js/jquery.jTinder.js
@@ -37,6 +37,30 @@
 		this.init(element);
 	}
 
+	function getNewPicture(){
+		var flickrUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=9b18b1255294c4fcd50c0d852e57e66d&tags=sexy%2Chot&sort=relevance&safe_search=3&content_type=1&per_page=500&format=json&nojsoncallback=1&auth_token=72157663647929405-c20ae865be786cbb&api_sig=3e2e1d230ff446a338fe2d54d052daeb"
+		$.ajax({ 
+		    type: "GET",
+		    dataType: "json",
+		    url: flickrUrl,
+		    success: function(data){
+		      var index = Math.floor(Math.random()*500);
+		      var photo = data.photos.photo[index];
+		      pullNewPicture(photo);
+		    },
+		    error: function(data){
+		      console.log(data);
+		    }
+		});
+	}
+
+	function pullNewPicture(photo){
+		var picUrl = "https://farm"+photo.farm+".staticflickr.com/"+ photo.server+"/"+photo.id+"_"+photo.secret+".jpg";
+		panes.eq(current_pane).find("img")[0].src = picUrl;
+		console.log(picUrl);
+		
+	}
+
 	function getTags(img,token){
 		var authbearer = "Bearer " + token;
 		console.log(token);
@@ -49,27 +73,26 @@
 	      data: {
 	        "url": img.src
 	      },
-	      success: function(data){
-	      	console.log(data);       
+	      success: function(data){     
 	        var tags = data.results[0].result.tag.classes;
-
+	        console.log(data);
 	        var url = "http://172.17.73.212:3000/" + status;
-			$.ajax({ 
-			    type: "POST",
-			    dataType: "jsonp",
-			    accepts: "application/json",
-			    contentType: "json",
-			    url: url,
-			    data: {
-			      "tags": tags
-			    },
-			    success: function(data){
-			      console.log(data);
-			    },
-			    error: function(data){
-			      console.log(data);
-			    }
-			});
+			// $.ajax({ 
+			//     type: "POST",
+			//     dataType: "jsonp",
+			//     accepts: "application/json",
+			//     contentType: "json",
+			//     url: url,
+			//     data: {
+			//       "tags": tags
+			//     },
+			//     success: function(data){
+			//       console.log(data);
+			//     },
+			//     error: function(data){
+			//       console.log(data);
+			//     }
+			// });
 		      },
 		      error: function(data){
 		        console.log(data);
@@ -141,6 +164,7 @@
 		like: function() {
 
 			sendTags("acceptedtags");
+			getNewPicture();
 		}
 
 	};
